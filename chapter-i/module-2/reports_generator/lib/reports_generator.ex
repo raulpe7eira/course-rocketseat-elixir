31bea19179b1d2af897bc07e54edc9be38f111ba
Raul Pereira <raul.pereira@gmail.com>
2021-03-09 21:21:22 -0300

    edit reports_generator (v1) : read file w/ stream

--- chapter-i/module-2/reports_generator/lib/reports_generator.ex
+++ chapter-i/module-2/reports_generator/lib/reports_generator.ex
@@ -1,10 +1,14 @@
 defmodule ReportsGenerator do
   def build(filename) do
     "reports/#{filename}"
-    |> File.read()
-    |> handle_file()
+    |> File.stream!()
+    |> Enum.map(fn line -> parse_line(line) end)
   end
 
-  defp handle_file({:ok, result}), do: result
-  defp handle_file({:error, reason}), do: reason
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
 end
