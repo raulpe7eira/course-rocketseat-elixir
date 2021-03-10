4aa11ef0c5c20ef907916fc9629e2cf756c70498
Raul Pereira <raul.pereira@gmail.com>
2021-03-09 21:06:07 -0300

    edit reports_generator (v1) : read file w/ pipe operator

--- chapter-i/module-2/reports_generator/lib/reports_generator.ex
+++ chapter-i/module-2/reports_generator/lib/reports_generator.ex
@@ -1,8 +1,10 @@
 defmodule ReportsGenerator do
   def build(filename) do
-    case File.read("reports/#{filename}") do
-      {:ok, result} -> result
-      {:error, reason} -> reason
-    end
+    "reports/#{filename}"
+    |> File.read()
+    |> handle_file()
   end
+
+  defp handle_file({:ok, result}), do: result
+  defp handle_file({:error, reason}), do: reason
 end
