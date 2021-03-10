f5589e5f44e2b5383171de7d0fc071abecda678a
Raul Pereira <raul.pereira@gmail.com>
2021-03-10 20:31:02 -0300

    edit reports_generator (v2) : add parallel version

--- chapter-i/module-2/reports_generator/lib/reports_generator.ex
+++ chapter-i/module-2/reports_generator/lib/reports_generator.ex
@@ -20,6 +20,12 @@ defmodule ReportsGenerator do
     |> Enum.reduce(init_report(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(init_report(), fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
   def fetch_higher_cost(report, option) when option in @options do
     {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
   end
@@ -30,15 +36,24 @@ defmodule ReportsGenerator do
     foods = Enum.into(@available_foods, %{}, &{&1, 0})
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
-    %{"users" => users, "foods" => foods}
+    build_report(foods, users)
   end
 
-  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users}) do
     foods = Map.put(foods, food_name, foods[food_name] + 1)
     users = Map.put(users, id, users[id] + price)
 
-    report
-    |> Map.put("foods", foods)
-    |> Map.put("users", users)
+    build_report(foods, users)
   end
+
+  defp sum_reports(%{"foods" => foods1, "users" => users1}, %{"foods" => foods2, "users" => users2}) do
+    foods = merge_maps(foods1, foods2)
+    users = merge_maps(users1, users2)
+
+    build_report(foods, users)
+  end
+
+  defp merge_maps(map1, map2), do: Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+
+  defp build_report(foods, users), do: %{"foods" => foods, "users" => users}
 end
