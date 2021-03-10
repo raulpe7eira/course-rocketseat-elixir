07363c23aa15ac760b6d40a31ea8e60f7573ba0c
Raul Pereira <raul.pereira@gmail.com>
2021-03-09 22:06:35 -0300

    edit reports_generator (v1) : returns max price

--- chapter-i/module-2/reports_generator/lib/reports_generator.ex
+++ chapter-i/module-2/reports_generator/lib/reports_generator.ex
@@ -7,6 +7,8 @@ defmodule ReportsGenerator do
     |> Enum.reduce(init_report(), fn line, report -> sum_values(line, report) end)
   end
 
+  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+
   defp init_report, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
   defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
