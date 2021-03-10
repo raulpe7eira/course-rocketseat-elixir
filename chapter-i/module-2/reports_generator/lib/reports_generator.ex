c1fa67004b62ca3e5d0cf2e7e8555353c5fd377e
Raul Pereira <raul.pereira@gmail.com>
2021-03-09 22:32:29 -0300

    edit reports_generator (v1) : report frequency by foods

--- chapter-i/module-2/reports_generator/lib/reports_generator.ex
+++ chapter-i/module-2/reports_generator/lib/reports_generator.ex
@@ -1,6 +1,17 @@
 defmodule ReportsGenerator do
   alias ReportsGenerator.Parser
 
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
@@ -9,7 +20,19 @@ defmodule ReportsGenerator do
 
   def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
 
-  defp init_report, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+  defp init_report do
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
-  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
+    %{"users" => users, "foods" => foods}
+  end
+
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+    users = Map.put(users, id, users[id] + price)
+
+    report
+    |> Map.put("foods", foods)
+    |> Map.put("users", users)
+  end
 end
