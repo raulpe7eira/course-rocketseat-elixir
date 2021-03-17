ff7613964add19154ae3075b6991d80ff41cccbf
Raul Pereira <raul.pereira@gmail.com>
2021-03-17 04:20:35 -0300

    exlivery: fix delegate

--- chapter-ii/module-3/exlivery/lib/exlivery.ex
+++ chapter-ii/module-3/exlivery/lib/exlivery.ex
@@ -1,7 +1,8 @@
 defmodule Exlivery do
   alias Exlivery.Orders.Agent, as: OrderAgent
+  alias Exlivery.Orders.CreateOrUpdate, as: CreateOrUpdateOrder
+  alias Exlivery.Orders.Report
   alias Exlivery.Users.Agent, as: UserAgent
-  alias Exlivery.Users.CreateOrUpdate, as: CreateOrUpdateOrder
   alias Exlivery.Users.CreateOrUpdate, as: CreateOrUpdateUser
 
   def start_agents do
@@ -11,4 +12,5 @@ defmodule Exlivery do
 
   defdelegate create_or_update_order(params), to: CreateOrUpdateOrder, as: :call
   defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_report_for_all_orders(filename), to: Report, as: :create
 end
