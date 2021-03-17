1f5bb1ac86080996c26dfdb001a4c125aa257eaa
Raul Pereira <raul.pereira@gmail.com>
2021-03-17 15:14:13 -0300

    flightex: add delegate features

--- chapter-ii/module-3/flightex/lib/flightex.ex
+++ chapter-ii/module-3/flightex/lib/flightex.ex
@@ -1,5 +1,16 @@
 defmodule Flightex do
-  def hello do
-    :world
+  alias Flightex.Bookings.Agent, as: BookingAgent
+  alias Flightex.Bookings.Create, as: BookingCreate
+  alias Flightex.Bookings.Get, as: BookingGet
+  alias Flightex.Users.Agent, as: UserAgent
+  alias Flightex.Users.Create, as: UserCreate
+
+  def start_agents do
+    BookingAgent.start_link(%{})
+    UserAgent.start_link(%{})
   end
+
+  defdelegate create_booking(user_id, params), to: BookingCreate, as: :call
+  defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate get_booking(booking_id), to: BookingGet, as: :call
 end
--- chapter-ii/module-3/flightex/test/flightex_test.exs
+++ chapter-ii/module-3/flightex/test/flightex_test.exs
@@ -1,7 +1,3 @@
 defmodule FlightexTest do
   use ExUnit.Case
-
-  test "greets the world" do
-    assert Flightex.hello() == :world
-  end
 end
