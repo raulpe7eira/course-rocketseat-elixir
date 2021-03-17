d39867adeecd4624c330e2fbe401a94c1e6f7a3d
Raul Pereira <raul.pereira@gmail.com>
2021-03-17 01:28:58 -0300

    exlivery: add order agent

---
+++ chapter-ii/module-3/exlivery/lib/orders/agent.ex
@@ -0,0 +1,27 @@
+defmodule Exlivery.Orders.Agent do
+  use Agent
+
+  alias Exlivery.Orders.Order
+
+  def start_link(_initial_state) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(%Order{} = order) do
+    uuid = UUID.uuid4()
+    Agent.update(__MODULE__, &update_state(&1, order, uuid))
+
+    {:ok, uuid}
+  end
+
+  defp update_state(state, %Order{} = order, uuid), do: Map.put(state, uuid, order)
+
+  def get(uuid), do: Agent.get(__MODULE__, &get_order(&1, uuid))
+
+  defp get_order(state, uuid) do
+    case Map.get(state, uuid) do
+      nil -> {:error, "Order not found"}
+      uuid -> {:ok, uuid}
+    end
+  end
+end
--- chapter-ii/module-3/exlivery/mix.exs
+++ chapter-ii/module-3/exlivery/mix.exs
@@ -24,7 +24,8 @@ defmodule Exlivery.MixProject do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:decimal, "~> 2.0"},
-      {:ex_machina, "~> 2.5.0"}
+      {:ex_machina, "~> 2.5.0"},
+      {:elixir_uuid, "~> 1.2"}
     ]
   end
 
