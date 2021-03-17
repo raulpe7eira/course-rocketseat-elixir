9c9e5d1872fa53c37691be46d7d340e06f26866a
Raul Pereira <raul.pereira@gmail.com>
2021-03-16 23:22:08 -0300

    exlivery: add validations for user struct

--- chapter-ii/module-3/exlivery/lib/users/user.ex
+++ chapter-ii/module-3/exlivery/lib/users/user.ex
@@ -1,3 +1,18 @@
 defmodule Exlivery.Users.User do
-  defstruct [:name, :email, :cpf, :age]
+  @keys [:name, :email, :cpf, :age]
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(name, email, cpf, age) when age >= 18 and is_bitstring(cpf) do
+    {:ok,
+     %__MODULE__{
+       name: name,
+       email: email,
+       cpf: cpf,
+       age: age
+     }}
+  end
+
+  def build(_name, _email, _cpf, _age), do: {:error, "Invalid parameters"}
 end
