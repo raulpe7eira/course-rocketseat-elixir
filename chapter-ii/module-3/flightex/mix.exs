edd3636870af54cc96ba672d39a9654b80fecbd3
Raul Pereira <raul.pereira@gmail.com>
2021-03-17 10:38:24 -0300

    add flightex project

---
+++ chapter-ii/module-3/flightex/mix.exs
@@ -0,0 +1,33 @@
+defmodule Flightex.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :flightex,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:elixir_uuid, "~> 1.2"},
+      {:ex_machina, "~> 2.5.0"}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+end
---
+++ chapter-ii/module-3/flightex/test/flightex_test.exs
@@ -0,0 +1,7 @@
+defmodule FlightexTest do
+  use ExUnit.Case
+
+  test "greets the world" do
+    assert Flightex.hello() == :world
+  end
+end
