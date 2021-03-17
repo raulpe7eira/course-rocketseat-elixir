51807eae64db6baae32364d97380ded04f37a8b4
Raul Pereira <raul.pereira@gmail.com>
2021-03-17 01:11:32 -0300

    exlivery: add user agent

---
+++ chapter-ii/module-3/exlivery/lib/users/agent.ex
@@ -0,0 +1,22 @@
+defmodule Exlivery.Users.Agent do
+  use Agent
+
+  alias Exlivery.Users.User
+
+  def start_link(_initial_state) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(%User{} = user), do: Agent.update(__MODULE__, &update_state(&1, user))
+
+  defp update_state(state, %User{cpf: cpf} = user), do: Map.put(state, cpf, user)
+
+  def get(cpf), do: Agent.get(__MODULE__, &get_user(&1, cpf))
+
+  defp get_user(state, cpf) do
+    case Map.get(state, cpf) do
+      nil -> {:error, "User not found"}
+      user -> {:ok, user}
+    end
+  end
+end
