b5341a8b690472f75161f59a350d6cf0e4e47d86
Raul Pereira <raul.pereira@gmail.com>
2021-03-23 22:19:06 -0300

    rockelivery: uses jason encoder in user schema

--- chapter-iii/module-4/rockelivery/lib/rockelivery_web/controllers/users_controller.ex
+++ chapter-iii/module-4/rockelivery/lib/rockelivery_web/controllers/users_controller.ex
@@ -7,7 +7,7 @@ defmodule RockeliveryWeb.UsersController do
     with {:ok, %User{} = user} <- Rockelivery.create_user(params) do
       conn
       |> put_status(:created)
-      |> render('create.json', user: user)
+      |> render("create.json", user: user)
     end
   end
 end
