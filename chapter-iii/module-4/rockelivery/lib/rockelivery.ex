c40d4306d3a368f024d4d318fbc8945e8c746fd8
Raul Pereira <raul.pereira@gmail.com>
2021-03-23 23:14:09 -0300

    rockelivery: add users delete

--- chapter-iii/module-4/rockelivery/lib/rockelivery.ex
+++ chapter-iii/module-4/rockelivery/lib/rockelivery.ex
@@ -1,7 +1,9 @@
 defmodule Rockelivery do
   alias Rockelivery.Users.Create, as: UserCreate
+  alias Rockelivery.Users.Delete, as: UserDelete
   alias Rockelivery.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate delete_user(id), to: UserDelete, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end
