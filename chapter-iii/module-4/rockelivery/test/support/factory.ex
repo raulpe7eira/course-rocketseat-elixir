b0bf74c08e2b5489737ba6cad4408835017076e8
Raul Pereira <raul.pereira@gmail.com>
2021-03-24 16:01:02 -0300

    rockelivery: add user controller test

--- chapter-iii/module-4/rockelivery/test/support/factory.ex
+++ chapter-iii/module-4/rockelivery/test/support/factory.ex
@@ -1,5 +1,7 @@
 defmodule Rockelivery.Factory do
-  use ExMachina
+  use ExMachina.Ecto, repo: Rockelivery.Repo
+
+  alias Rockelivery.User
 
   def user_params_factory do
     %{
@@ -10,6 +12,19 @@ defmodule Rockelivery.Factory do
       email: "raul@mail.com",
       password: "123456",
       name: "Raul Pereira"
+    }
+  end
+
+  def user_factory do
+    %User{
+      age: 22,
+      address: "Rua X, 123",
+      cep: "12345678",
+      cpf: "47971092032",
+      email: "raul@mail.com",
+      password: "123456",
+      name: "Raul Pereira",
+      id: "27d34263-6b3f-4236-a2a1-3949c3804b93"
     }
   end
 end
