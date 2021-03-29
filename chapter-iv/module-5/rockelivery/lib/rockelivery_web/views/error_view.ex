9ec9c163b554e52090b03252eaae29b368751f66
Raul Pereira <raul.pereira@gmail.com>
2021-03-29 13:55:37 -0300

    rockelivery: fix category error

--- chapter-iv/module-5/rockelivery/lib/rockelivery_web/views/error_view.ex
+++ chapter-iv/module-5/rockelivery/lib/rockelivery_web/views/error_view.ex
@@ -29,8 +29,12 @@ defmodule RockeliveryWeb.ErrorView do
   defp translate_errors(changeset) do
     traverse_errors(changeset, fn {msg, opts} ->
       Enum.reduce(opts, msg, fn {key, value}, acc ->
-        String.replace(acc, "%{#{key}", to_string(value))
+        String.replace(acc, "%{#{key}", translate_value(value))
       end)
     end)
   end
+
+  defp translate_value({:parameterized, Ecto.Enum, _map}), do: ""
+
+  defp translate_value(value), do: to_string(value)
 end
