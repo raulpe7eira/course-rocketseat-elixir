e4454c254e956ef2ac81ce7b709cc470feceb921
Raul Pereira <raul.pereira@gmail.com>
2021-03-29 17:36:24 -0300

    rockelivery: refactories in create order

--- chapter-iv/module-5/rockelivery/lib/rockelivery/orders/create.ex
+++ chapter-iv/module-5/rockelivery/lib/rockelivery/orders/create.ex
@@ -2,43 +2,18 @@ defmodule Rockelivery.Orders.Create do
   import Ecto.Query
 
   alias Rockelivery.{Error, Item, Order, Repo}
+  alias Rockelivery.Orders.ValidateAndMultiplyItems
 
-  def call(params) do
-    params
-    |> fetch_items()
-    |> handle_items(params)
-  end
-
-  defp fetch_items(%{"items" => items_params}) do
+  def call(%{"items" => items_params} = params) do
     items_ids = Enum.map(items_params, fn item -> item["id"] end)
 
     query = from item in Item, where: item.id in ^items_ids
 
     query
     |> Repo.all()
-    |> validate_and_multiply_items(items_ids, items_params)
+    |> ValidateAndMultiplyItems.call(items_ids, items_params)
+    |> handle_items(params)
   end
-
-  defp validate_and_multiply_items(items, items_ids, items_params) do
-    items_map = Map.new(items, fn item -> {item.id, item} end)
-
-    items_ids
-    |> Enum.map(fn id -> {id, Map.get(items_map, id)} end)
-    |> Enum.any?(fn {_id, value} -> is_nil(value) end)
-    |> multiply_items(items_map, items_params)
-  end
-
-  defp multiply_items(false, items, items_params) do
-    items =
-      Enum.reduce(items_params, [], fn %{"id" => id, "quantity" => quantity}, acc ->
-        item = Map.get(items, id)
-        acc ++ List.duplicate(item, quantity)
-      end)
-
-    {:ok, items}
-  end
-
-  defp multiply_items(true, _items, _items_params), do: {:error, "Invalid ids!"}
 
   defp handle_items({:ok, items}, params) do
     params
