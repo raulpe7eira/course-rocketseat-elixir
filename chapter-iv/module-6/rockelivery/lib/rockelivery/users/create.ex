951b5e0029d3983bad8586afdb709db68ebaf1cb
Raul Pereira <raul.pereira@gmail.com>
2021-03-31 00:09:15 -0300

    rockelivery: fix any tests

--- chapter-iv/module-6/rockelivery/lib/rockelivery/users/create.ex
+++ chapter-iv/module-6/rockelivery/lib/rockelivery/users/create.ex
@@ -2,7 +2,9 @@ defmodule Rockelivery.Users.Create do
   alias Rockelivery.{Error, Repo, User}
   alias Rockelivery.ViaCep.Client
 
-  def call(%{"cep" => cep} = params) do
+  def call(params) do
+    cep = Map.get(params, "cep")
+
     changeset = User.changeset(params)
 
     with {:ok, %User{}} <- User.build(changeset),
