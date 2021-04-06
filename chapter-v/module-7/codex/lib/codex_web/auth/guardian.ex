2b53531debc896fc057679b9da953d3f08cf1883
Raul Pereira <raul.pereira@gmail.com>
2021-04-06 18:46:07 -0300

    codex: update token with ttl and add refresh token support service

--- chapter-v/module-7/codex/lib/codex_web/auth/guardian.ex
+++ chapter-v/module-7/codex/lib/codex_web/auth/guardian.ex
@@ -5,6 +5,8 @@ defmodule CodexWeb.Auth.Guardian do
   alias Codex.Users.User
   alias Codex.Users.Retrieve, as: RetrieveUser
 
+  @ttl_login {1, :minute}
+
   def subject_for_token(%User{id: id}, _claims), do: {:ok, id}
 
   def resource_from_claims(claims) do
@@ -19,7 +21,7 @@ defmodule CodexWeb.Auth.Guardian do
   def authenticate(%{"id" => user_id, "password" => password}) do
     with {:ok, %User{password_hash: hash} = user} <- RetrieveUser.call(user_id),
          true <- Pbkdf2.verify_pass(password, hash),
-         {:ok, token, _claims} <- encode_and_sign(user) do
+         {:ok, token, _claims} <- encode_and_sign(user, %{}, ttl: @ttl_login) do
       {:ok, token}
     else
       false -> {:error, Error.build(:unauthorized, "Please verify your credentials!")}
@@ -28,4 +30,14 @@ defmodule CodexWeb.Auth.Guardian do
   end
 
   def authenticate(_), do: {:error, Error.build(:bad_request, "Invalid or missing params!")}
+
+  def refresh_token(%{"token" => token}) do
+    with {:ok, _old_stuff, {new_token, _new_claims}} <- refresh(token, ttl: @ttl_login) do
+      {:ok, new_token}
+    else
+      {:error, reason} -> {:error, Error.build(:unauthorized, reason)}
+    end
+  end
+
+  def refresh_token(_), do: {:error, Error.build(:bad_request, "Invalid or missing params!")}
 end
