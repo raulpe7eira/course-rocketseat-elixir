6024db66f643f9c08ca02a797fc5b3d00b17d2a6
Raul Pereira <raul.pereira@gmail.com>
2021-04-06 11:00:16 -0300

    codex: improves migration

--- chapter-v/module-7/codex/priv/repo/migrations/20210406000048_create_users_table.exs
+++ chapter-v/module-7/codex/priv/repo/migrations/20210406000048_create_users_table.exs
@@ -3,7 +3,7 @@ defmodule Codex.Repo.Migrations.CreateUsersTable do
 
   def change do
     create table :users do
-      add :password_hash, :string
+      add :password_hash, :string, null: false
 
       timestamps()
     end
