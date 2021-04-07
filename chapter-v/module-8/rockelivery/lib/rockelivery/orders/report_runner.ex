f9903fe2af1cb4cbcd3ff1fda75b62f3e013f826
Raul Pereira <raul.pereira@gmail.com>
2021-04-06 22:04:41 -0300

    rockelivery: add report runner

---
+++ chapter-v/module-8/rockelivery/lib/rockelivery/orders/report_runner.ex
@@ -0,0 +1,35 @@
+defmodule Rockelivery.Orders.ReportRunner do
+  use GenServer
+
+  require Logger
+
+  alias Rockelivery.Orders.Report
+
+  # Client
+
+  def start_link(_initial_stack), do: GenServer.start_link(__MODULE__, %{})
+
+  # Server
+
+  @impl true
+  def init(state) do
+    schedule_report_generation()
+
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:generate, state) do
+    Logger.info("Generating report...")
+
+    Report.create()
+
+    schedule_report_generation()
+
+    {:noreply, state}
+  end
+
+  def schedule_report_generation do
+    Process.send_after(self(), :generate, 1000 * 10)
+  end
+end
